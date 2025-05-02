e047fb4ac60a3487f9f0a33cfca7ccd5e170843f
OtakuForLife <lukas.kuehl@protonmail.com>
2025-05-02 14:26:43 +0200

    add csrf token retrieving on frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -13,30 +13,44 @@ import PrivateRoute from './components/PrivateRoute'
 import Navbar from './components/Navbar'
 import { useAppDispatch } from './hooks/useAppDispatch'
 import { loadTheme } from './store/slices/themeSlice'
+import api, { setCsrfToken } from './utils/api'
 
 // Create a component to load user data
 const AppContent = () => {
   const dispatch = useAppDispatch()
   
   useEffect(() => {
-    dispatch(loadUser())
-    dispatch(loadTheme())
+    // Get CSRF token first
+    api.get('/account/csrf/')
+      .then(response => {
+        if (response.data && response.data.csrfToken) {
+          setCsrfToken(response.data.csrfToken)
+          // After getting CSRF token, load user data
+          dispatch(loadUser())
+          dispatch(loadTheme())
+        }
+      })
+      .catch(error => {
+        console.error('Error fetching CSRF token:', error)
+      })
   }, [dispatch])
-  
+
   return (
-    <div className="min-h-screen bg-gray-100">
-      <Navbar />
-      <main className="container mx-auto px-4 py-8">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
-          <Route path="/stories/:id" element={<PrivateRoute><StoryDetailPage /></PrivateRoute>} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Routes>
-      </main>
-    </div>
+    <>
+      <div className="min-h-screen bg-gray-100">
+        <Navbar />
+        <main className="container mx-auto px-4 py-8">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>} />
+            <Route path="/stories/:id" element={<PrivateRoute><StoryDetailPage /></PrivateRoute>} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+        </main>
+      </div>
+    </>
   )
 }
 
@@ -52,4 +66,7 @@ function App() {
 }
 
 export default App
+
+
+
 
--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -9,4 +9,21 @@ const api = axios.create({
   withCredentials: true // This is important for sending cookies with requests
 });
 
+// Store the CSRF token
+let csrfToken = '';
+
+// Function to set the CSRF token
+export const setCsrfToken = (token: string) => {
+  csrfToken = token;
+};
+
+// Add a request interceptor to include CSRF token in all non-GET requests
+api.interceptors.request.use(config => {
+  // Only add CSRF token for non-GET requests
+  if (config.method !== 'get' && csrfToken) {
+    config.headers['X-Csrftoken'] = csrfToken;
+  }
+  return config;
+});
+
 export default api;
